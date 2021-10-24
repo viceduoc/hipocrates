ee58f17f6a4b95cec78a4da631d5e90e24ca2787
Gustavo Diaz <gustavo.ianiszewski@gmail.com>
2021-10-23 22:24:44 -0300

    def func

--- clinica/views.py
+++ clinica/views.py
@@ -1,7 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
+
 
 # Create your views here.
 
 
 def home(request):
-    return render (request, 'clinica/home.html')
+    return render(request, 'clinica/home.html')
+
+
+def prueba(request):
+    contexto = {
+        "especialidad": "Medicina general"
+    }
+    return render(request, 'template/clinica/home.html', contexto)
+
+def test(request):
+    lista=[ "Lasaña", "Porotos"]
+    contexto = {"especialidad": "Medicina general", "comidas": lista}
+    return render(request, 'template/clinica/home.html', contexto)
