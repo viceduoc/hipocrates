32dfe704d87ea08687fc0b027922c8d588934b0d
vicente carmona <vice.carmona@duocuc.cl>
2021-10-24 22:26:23 -0300

    adds models funcionario, seguro, paciente and reserva

--- clinica/models.py
+++ clinica/models.py
@@ -30,3 +30,38 @@ class Funcionario(models.Model):
     def __str__(self):
         return self.nombre
 
+class Seguro(models.Model):
+    idSeguro = models.IntegerField(primary_key=True, verbose_name="ID seguro")
+    seguro = models.CharField(max_length=60, verbose_name="nombre seguro")
+
+    def __str__(self):
+        return self.seguro
+
+class Paciente(models.Model):
+    idPaciente = models.IntegerField(primary_key=True, verbose_name="id paciente")
+    rut = models.IntegerField(max_length=12, verbose_name="rut del paciente")
+    nombre = models.CharField(max_length=80, verbose_name="nombre del paciente")
+    correo = models.CharField(max_length=60, verbose_name="email del paciente")
+    password =models.CharField(max_length=8, verbose_name="password del paciente")
+    debe_actualizar = models.BooleanField(verbose_name="contraseña provisoria")
+    seguro = models.ForeignKey(Seguro, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nombre
+
+class Reserva(models.Model):
+    idReserva = models.IntegerField(primary_key=True, verbose_name="ID reserva")
+    fechaHoraReserva = models.DateTimeField(verbose_name="Fecha reserva")
+    estaConfirmada = models.BooleanField(verbose_name="Está confirmada?")
+    estaCompletada = models.BooleanField(verbose_name="Está completada?")
+    funcionario = models.ForeignKey(Funcionario, on_delete=models.CASCADE)
+    paciente = models.ForeignKey(Paciente, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.funcionario + " " + self.paciente
+
+
+
+
+
+
