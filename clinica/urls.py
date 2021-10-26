e375eb2b925cde968e1d9cd18824d066a17ef576
Patricio Mardones <pa.mardonesg@duocuc.cl>
2021-10-25 23:15:23 -0300

    menureservas

--- clinica/urls.py
+++ clinica/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import home, reserva
+from .views import home, reserva, crearreserva
 
 urlpatterns = [
     path('', home, name = "home"),
     path('reserva.html', reserva, name="reserva"),
+    path('crear-reserva.html', crearreserva, name="crear-reserva"),
 
 
 ]
